e75a791fae05a7c1fbbd61e893d46c74e4cd3ca4
Terrokz <antek727@gmail.com>
2022-03-15 03:07:51 +0100

    po sekcji 3

---
+++ src/data/FeedbackData.js
@@ -0,0 +1,13 @@
+const FeedbackData = [
+    {
+        id: 1,
+        rating: 10,
+        text: 'jakis tam text1 pierwszy'
+    },
+    {
+        id: 2,
+        rating: 9,
+        text: 'drugi tekst recenzji feedback'
+    }
+]
+export default FeedbackData
